1e3d8b0fdab2bb319616675127f0e7399d304788
Zachary Stence <zachstence@gmail.com>
2022-03-31 18:11:27 -0500

    feat: read DHT22 value

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+import { promises as sensor } from "node-dht-sensor";
+
+const TYPE = 22;
+const PIN = 14;
+
+const run = async () => {
+    try {
+        const {temperature, humidity} = await sensor.read(TYPE, PIN);
+
+        const timeStr = new Date().toLocaleString();
+        const tempStr = `${temperature.toFixed(1)}°C`;
+        const humStr = `${humidity.toFixed(1)}%`;
+        console.log(`${timeStr}\t${tempStr}\t${humStr}`);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+setInterval(run, 5000);
